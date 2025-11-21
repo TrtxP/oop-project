717c2ecb116f16dfe5954fd115fd0c242394cbb2
Ilya Cherepanov <zipz241_chii@student.ztu.edu.ua>
2025-11-21 19:46:21 +0200

    All added to repository

---
+++ lab3/ClassLibraryStr/String.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibraryStr
+{
+    public class String
+    {
+        public static string StringInvert(string str)
+        {
+            char[] charArr = str.ToCharArray();
+            Array.Reverse(charArr);
+            return new string(charArr);
+        }
+
+        public static int CountChars(string str, char ch)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == ch)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
